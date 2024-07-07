f182e77e8fd03a2d992d4e6bb188201c032db051
bryanmsombra27 <bryanjm96@gmail.com>
2024-07-07 15:52:10 -0600

    working on routes

--- helpers.php
+++ helpers.php
@@ -1,6 +1,4 @@
 <?php
-
-
 /**
  * Get the base path
  * 
@@ -8,6 +6,59 @@
  * @return string 
  */
 function basePath($path = ''){
+    $filePath = __DIR__ . '/' . $path;
 
-    return __DIR__ . '/' . $path;
+    return $filePath;
+}
+
+/**
+ * Load a view 
+ * @param string $name
+ * @return void
+ */
+function loadView($name){
+    $viewpath = basePath("views/{$name}.view.php");
+    if(file_exists(($viewpath))){
+        require $viewpath;
+    }else{
+        echo "View {$name} not found!";
+    }
+
+}
+/**
+ * Load a partial 
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name){
+    $viewpath = basePath("views/partials/{$name}.php");
+
+    if(file_exists(($viewpath))){
+        require $viewpath;
+    }else{
+        echo "View {$name} not found!";
+    }
+}
+
+
+/**
+ * Inspect a value(s)
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value){
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+/**
+ * Inspect a value(s) and die
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value){
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+    die();
 }
